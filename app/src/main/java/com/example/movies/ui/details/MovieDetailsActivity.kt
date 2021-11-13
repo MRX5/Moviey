68e2f381cedb5848cb0e7acd6f17dc7530be8aae
Mostafa Gad <m.gad192@gmial.com>
2021-11-13 15:38:10 +0200

    commit

---
+++ app/src/main/java/com/example/movies/ui/details/MovieDetailsActivity.kt
@@ -0,0 +1,12 @@
+package com.example.movies.ui.details
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.movies.R
+
+class MovieDetailsActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_movie_details)
+    }
+}
